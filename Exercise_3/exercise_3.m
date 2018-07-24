5e003a4aff42f01ea26ca894cebb907e81a2a1cd
Simon Diehl <j.simon.diehl@googlemail.com>
2018-07-24 16:16:14 +0200

    Added observations and template for exercise 3

---
+++ Exercise_3/exercise_3.m
@@ -0,0 +1,33 @@
+% pr_exercise_3.m
+
+% Read again the sescirption and answer the following:
+% 
+% 1. Based on your observations are all the selected featured needed for correct classifications?
+%
+% 2. Based on the assumptiom that the selected features are conditionally independent classify the observations
+%
+% 3. Visualize the risk of a wrong decision for observetion 5 by varying the coordinates under the assumption of a Zero-One loss model
+
+% load data
+observations = importdata('pr_exercise_3_observations.txt', ',', 1);
+
+% Task 1
+%
+% (code)
+%
+
+% Task 2
+observations = [42.12 10.43 37.7 5.0 2.1;
+                49.82 22.89 38.6 3.4 3.9;
+                58.90 18.71 40.1 2.2 7.7;
+                32.85 06.35 38.2 8.6 1.0;
+                54.56 22.43 38.3 9.3 9.0;
+                42.85 -0.35 36.2 1.6 4.2];
+%
+% (code)
+%
+
+% Task 3
+%
+% (code)
+%
