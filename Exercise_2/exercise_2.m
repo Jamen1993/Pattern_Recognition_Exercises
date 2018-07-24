a9d9b1e33093b0e589c0555a7e2bb3e63453eb97
Simon Diehl <j.simon.diehl@googlemail.com>
2018-07-24 14:52:40 +0200

    Finished task a

--- Exercise_2/exercise_2.m
+++ Exercise_2/exercise_2.m
@@ -1,28 +1,46 @@
 % exercise 2
-% In this exercise we want to find how well a binary classifier works in 
+% In this exercise we want to find how well a binary classifier works in
 % detecting above normal values from two interleaved normal distributions.
 % 1. Plot the ROC curve for the decision rule
 % 2. Plot the Precision-Recall curve of the same data
 % 3. Plot the cost curve for the provided costs of different types of mistakes
 % 4. Find the optimal point of operation
 
-% normal distribution classes
-rng(0);
-numOfObservations = 1000;
-mean1 = 3;
-std1 = 2;
-mean2 = 7;
-std2= 2;
-normal = sort(normrnd(mean1, std1, [1 numOfObservations]), 'descend');
-aboveNormal = sort(normrnd(mean2, std2, [1 numOfObservations]), 'descend');
+% Read observations from file
+data = readtable('observations.csv');
+normal = data.normal;
+above_normal = data.aboveNormal;
 
-% Calculate and plot the ROC curve below
-% 
-% (code)
-%
+% Calculate and plot the ROC curve
+min_threshold = floor(min([normal; above_normal]));
+max_threshold = ceil(max([normal; above_normal]));
+thresholds = linspace(min_threshold, max_threshold);
 
+sensitivity = zeros(100, 1);
+specificity = zeros(100, 1);
 
-% Calculate precision, recall and plot curve below
+for it = 1:100
+    threshold = thresholds(it);
+
+    classified_normal = normal > threshold;
+    classified_above_normal = above_normal > threshold;
+
+    TP = sum(classified_above_normal);
+    % FP = sum(classified_normal);
+    TN = sum(~classified_normal);
+    % FN = sum(~classified_above_normal);
+
+    sensitivity(it) = TP / length(above_normal);
+    specificity(it) = TN / length(normal);
+end
+
+plot(1 - specificity, sensitivity);
+title('ROC');
+xlabel('1 - specificity / FPR');
+ylabel('sensitivity / TPR');
+grid on;
+
+% Calculate precision, recall and plot curve
 %
 % (code)
 %
